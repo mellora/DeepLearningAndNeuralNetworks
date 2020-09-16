85031f994d98843824ed19e702af1dbf108a90f2
Alexander Mellor <AlexMellor2014@gmail.com>
2020-09-16 18:40:55 -0500

    Added/modified code from tutorial

--- src/P_06.py
+++ src/P_06.py
@@ -2,6 +2,10 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm  # Gives a progress bar
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+from abc import ABC
 
 REBUILD_DATA = False
 
@@ -30,12 +34,46 @@ class DogsVsCats:
                     elif label == self.DOGS:
                         self.dog_count += 1
                 except Exception as e:
-                    # print(str(e))
                     pass
             np.random.shuffle(self.training_data)
             np.save("training_data.npy", self.training_data)
             print(f"Cats: {self.cat_count}")
             print(f"Dogs: {self.dog_count}")
+
+
+class Net(nn.Module, ABC):
+    def __init__(self):
+        super().__init__()
+        # .Conv2d is input, output, kernel size
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50, 50).view(-1, 1, 50, 50)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 2)
+
+    def convs(self, x):
+        x = func.max_pool2d(func.relu(self.conv1(x)), (2, 2))
+        x = func.max_pool2d(func.relu(self.conv2(x)), (2, 2))
+        x = func.max_pool2d(func.relu(self.conv3(x)), (2, 2))
+
+        # print(x[0].shape)
+        x = torch.flatten(x, 1, -1)  # Flatten
+        if self._to_linear is None:
+            # self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+            self._to_linear = x.shape[1]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        return func.softmax(x, dim=1)
 
 
 if __name__ == "__main__":
@@ -44,8 +82,4 @@ if __name__ == "__main__":
         dogs_v_cats.make_training_data()
     else:
         training_data = np.load("training_data.npy", allow_pickle=True)
-        print(len(training_data))
-        print(training_data[1])
-        import matplotlib.pyplot as plt
-        plt.imshow(training_data[1][0], cmap="gray")
-        plt.show()
+        net = Net()
