9515b4a45293fd65c2e3d68fc6f1a3b3c4a326f8
Alexander Mellor <AlexMellor2014@gmail.com>
2020-09-21 16:43:01 -0500

    Fixed a type in the code and added support to epoch

--- src/PyTorchGraph.py
+++ src/PyTorchGraph.py
@@ -3,7 +3,7 @@ from matplotlib import style
 
 style.use("ggplot")
 
-model_name = "model-1600454246"
+model_name = "model-1600724085"
 
 
 def create_acc_loss_graph(model_name):
@@ -18,10 +18,12 @@ def create_acc_loss_graph(model_name):
 
     for c in contents:
         if model_name in c:
-            name, timestamp, acc, loss, val_acc, val_loss = c.split(",")
+            name, timestamp, acc, loss, val_acc, val_loss, epoch = c.split(",")
+
             times.append(float(timestamp))
             accuracies.append(float(acc))
             losses.append(float(loss))
+
             val_accs.append(float(val_acc))
             val_losses.append(float(val_loss))
 
@@ -29,15 +31,15 @@ def create_acc_loss_graph(model_name):
 
     ax1 = plt.subplot2grid((2, 1), (0, 0))
     ax2 = plt.subplot2grid((2, 1), (1, 0), sharex=ax1)
-    
+
     ax1.plot(times, accuracies, label="acc")
-    ax1.plot(times, val_acc, label="val_acc")
+    ax1.plot(times, val_accs, label="val_acc")
     ax1.legend(loc=2)
-    
+
     ax2.plot(times, losses, label="loss")
-    ax2.plot(times, losses, label="val_loss")
+    ax2.plot(times, val_losses, label="val_loss")
     ax2.legend(loc=2)
-    
+
     plt.show()
 
 
